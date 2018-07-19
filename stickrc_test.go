96e659e99432fdf501528f352d987a323d344877
Alexey Grebenshchikov <mi.aleksio@gmail.com>
2018-07-19 09:22:24 +0300

    Tests for StickRC

--- stickrc_test.go
+++ stickrc_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"bytes"
 )
 
 func TestNewPacket(t *testing.T) {
@@ -27,5 +28,15 @@ func TestRCPacket_SetCommand(t *testing.T) {
 	p.SetCommand(SceneOff)
 	if p.Command != 0 {
 		t.Error("Wrong constant values")
+	}
+}
+
+func TestRCPacket_GetBytes(t *testing.T) {
+	b := []byte{0x53, 0x74, 0x69, 0x63, 0x6b, 0x5f, 0x33, 0x41, 0x6d, 0x00, 0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+	p := NewRCPacket()
+	p.SetScene(0, 5)
+	p.SetCommand(SceneOn)
+	if bytes.Compare(b, p.GetBytes()) !=0 {
+		t.Error("Wrong bytes slice created from RCPacket")
 	}
 }
